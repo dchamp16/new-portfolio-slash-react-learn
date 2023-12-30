575ac55f5e062be7fd1852642b872d612e92f4fd
Peter Ramos <86265240+dchamp16@users.noreply.github.com>
2023-12-30 17:43:40 +0000

    moved the comment inline for readable code

--- src/componets/Test-UseEffect.jsx
+++ src/componets/Test-UseEffect.jsx
@@ -1,51 +1,48 @@
 import React, { useState } from "react";
-import useFetchData from './hooks/useFetchData'
+import useFetchData from "./hooks/useFetchData";
 
 const TestUseEffect = () => {
-    // Tracks the text typed by the user
-    const [inputValue, setInputValue] = useState("");
-    // Custom hook to fetch posts and track loading/error status
-    const { data, loading, error } = useFetchData("https://jsonplaceholder.typicode.com/posts");
+  const [inputValue, setInputValue] = useState(""); // Tracks the text typed by the user
+  const { data, loading, error } = useFetchData(
+    "https://jsonplaceholder.typicode.com/posts"
+  ); // Custom hook to fetch posts and track loading/error status
 
-    // Updates inputValue with what the user types
-    function handleInputChange(event) {
-        setInputValue(event.target.value);
-    }
+  function handleInputChange(event) {
+    setInputValue(event.target.value); // Updates inputValue with what the user types
+  }
 
+  function handleRender() {
     // Displays data, loading status, or error to the user
-    function handleRender() {
-        // If still fetching data, let the user know
-        if (loading) {
-            alert('Please wait, the data is loading...');
-            return;
-        }
-
-        // If there was a problem fetching data, show the error
-        if (error) {
-            alert('Sorry, there was a problem: ' + error.message);
-            return;
-        }
-
-        // Picks a random post and checks if the user's input matches a property
-        const randomIndex = Math.floor(Math.random() * data.length);
-        const randomPost = data[randomIndex];
-
-        // Shows the property value if it exists, otherwise informs the user
-        if (inputValue in randomPost) {
-            alert(randomPost[inputValue]);
-        } else {
-            alert("The property you entered doesn't exist.");
-        }
+    if (loading) {
+      alert("Please wait, the data is loading..."); // If still fetching data, let the user know
+      return;
     }
 
+    if (error) {
+      alert("Sorry, there was a problem: " + error.message); // If there was a problem fetching data, show the error
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * data.length); // Picks a random post
+    const randomPost = data[randomIndex];
+
+    if (inputValue in randomPost) {
+      alert(randomPost[inputValue]); // Shows the property value if it exists
+    } else {
+      alert("The property you entered doesn't exist."); // Otherwise informs the user
+    }
+  }
+
+  return (
     // The user interface of the component
-    return (
-        <>
-            <p>Please enter one of the following properties: userId, id, title, body</p>
-            <input type="text" value={inputValue} onChange={handleInputChange}/>
-            <button onClick={handleRender}>Check Property</button>
-        </>
-    );
+    <>
+      <p>
+        Please enter one of the following properties: userId, id, title, body
+      </p>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleRender}>Check Property</button>
+    </>
+  );
 };
 
 export default TestUseEffect;
--- src/componets/hooks/useFetchData.jsx
+++ src/componets/hooks/useFetchData.jsx
@@ -1,36 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function useFetchData(url) {
-    // State for storing fetched data
-    const [data, setData] = useState(null);
-    // State to indicate loading process
-    const [loading, setLoading] = useState(true);
-    // State to capture any errors during fetch
-    const [error, setError] = useState(null);
+  const [data, setData] = useState(null); // State for storing fetched data
+  const [loading, setLoading] = useState(true); // State to indicate loading process
+  const [error, setError] = useState(null); // State to capture any errors during fetch
 
-    useEffect(() => {
-        // Function to fetch data from the provided URL
-        const fetchData = async () => {
-            setLoading(true); // Begin loading state
-            try {
-                const response = await fetch(url); // Attempt to fetch data from URL
-                if (!response.ok) {
-                    // If response is not 2xx, throw an error
-                    throw new Error(`Network response was not ok: ${response.statusText}`);
-                }
-                const json = await response.json(); // Convert response to JSON
-                setData(json); // Set the data in state
-            } catch (error) {
-                setError(error); // If error, set the error in state
-            } finally {
-                setLoading(false); // Finish loading state
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      // Function to fetch data from the provided URL
+      setLoading(true); // Begin loading state
+      try {
+        const response = await fetch(url); // Attempt to fetch data from URL
+        if (!response.ok) {
+          // If response is not 2xx, throw an error
+          throw new Error(
+            `Network response was not ok: ${response.statusText}`
+          );
+        }
+        const json = await response.json(); // Convert response to JSON
+        setData(json); // Set the data in state
+      } catch (error) {
+        setError(error); // If error, set the error in state
+      } finally {
+        setLoading(false); // Finish loading state
+      }
+    };
 
-        fetchData(); // Call the fetchData function
-    }, [url]); // Only re-run the effect if the URL changes
+    fetchData(); // Call the fetchData function
+  }, [url]); // Only re-run the effect if the URL changes
 
-    return { data, loading, error }; // Return the states
+  return { data, loading, error }; // Return the states
 }
 
 export default useFetchData;
